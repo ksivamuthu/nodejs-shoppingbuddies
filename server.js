3a68e2709f439e9e9dcfe8e7691d3d78da82c4be
Siva <sivamuthukumar07@gmail.com>
2014-07-04 18:49:48 +0530

    no message

--- server.js
+++ server.js
@@ -178,7 +178,7 @@ function updateTripStatus (req,res,next) {
    res.setHeader('Access-Control-Origin','*');
     var data = req.body;
     var params = data.userId + "," + data.tripId + "," + data.status;
-    connection.query('Update TRIP_ATTENDEES SET STATUS =' + data.status + ', RESPONDED = 1 WHERE ATTENDEEID =' + userId +'and TRIPID = ' +tripId +';',function(err, result) {
+    connection.query('Update TRIP_ATTENDEES SET STATUS =' + data.status + ', RESPONDED = 1 WHERE ATTENDEEID =' + data.userId +'and TRIPID = ' + data.tripId +';',function(err, result) {
         if(err){
             res.send(200,{error: err});
         } else {
